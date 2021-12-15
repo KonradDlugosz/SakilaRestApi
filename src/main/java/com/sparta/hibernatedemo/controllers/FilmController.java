7c257e14e8a82a3a0624ad3369053097c81d0764
Nikos Papadopoulos <MoodHunter34>
2021-12-15 12:11:08 +0000

    adding controller

--- src/main/java/com/sparta/hibernatedemo/controllers/FilmController.java
+++ src/main/java/com/sparta/hibernatedemo/controllers/FilmController.java
@@ -1,5 +1,21 @@
 package com.sparta.hibernatedemo.controllers;
 
+import com.sparta.hibernatedemo.entities.Film;
+import com.sparta.hibernatedemo.repositories.FilmRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class FilmController {
-    
+
+    @Autowired
+    private FilmRepository filmRepository;
+
+    @GetMapping(value = "/sakila/films")
+    public List<Film> getFilms(){
+        return filmRepository.findAll();
+    }
 }
