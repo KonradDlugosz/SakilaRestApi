ccaa060321cda86b10254a81338048b4684b65e3
KonradDlugosz <56347935+KonradDlugosz@users.noreply.github.com>
2021-12-16 15:58:28 +0000

    Added test package

---
+++ client/src/test/java/StaffTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
+public class StaffTest {
+
+    @Test
+    public void getStaffWithIdOne(){
+
+        String getOneStaffUrl = "http://localhost:8080/sakila/staff/one?id=1";
+
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(getOneStaffUrl)).build();
+
+    }
+
+}
