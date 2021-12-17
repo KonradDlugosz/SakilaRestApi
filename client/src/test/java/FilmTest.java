32dab763e205b1af6fe5a59c51b7f48e5dacf30b
Nikos Papadopoulos <MoodHunter34>
2021-12-17 14:23:41 +0000

    fixed merge conflicts

--- client/src/test/java/FilmTest.java
+++ client/src/test/java/FilmTest.java
@@ -2,67 +2,54 @@
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.sakila.entity.Film;
+import com.sparta.sakila.entity.Staff;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Path;
+
+import static com.sprata.sakila.FilmRequests.*;
 
 public class FilmTest {
 
     public static Film film;
+    public static Film postfilm;
+    public static Film putFilm;
 
-    public static HttpResponse<String> getOneFilmConnection() {
-        String getOneFilmUrl = "http://localhost:8080/sakila/films/4";
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(getOneFilmUrl)).build();
-        HttpClient client = HttpClient.newHttpClient();
-        HttpResponse<String> response = null;
-        try {
-            response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-        return response;
-    }
-
-    public static Film getOneFilmJasonBody() {
-        String json = getOneFilmConnection().body();
-        ObjectMapper mapper = new ObjectMapper();
-        Film film = null;
-        try {
-            film = mapper.readValue(json, Film.class);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        return film;
-    }
 
     @BeforeAll
     public static void getFilms() {
         film = getOneFilmJasonBody();
+        postfilm = postNewFilm();
+        putFilm = putNewFilm();
     }
 
     @Test
-    @DisplayName("Given id 4, return id")
+    @DisplayName("Given id 5, return id")
     public void getFilmIdTest() {
-        Assertions.assertEquals(4, film.getId());
+        Assertions.assertEquals(5, film.getId());
     }
 
     @Test
-    @DisplayName("Given a title of id 4, return film title")
+    @DisplayName("Given a title of id 5, return film title")
     public void getFilmTitleTest() {
-        Assertions.assertEquals("AFFAIR PREJUDICE", film.getTitle());
+        Assertions.assertEquals("AFRICAN EGG", film.getTitle());
     }
 
     @Test
     @DisplayName("Given a descritpion, return a description")
     public void getDescriptionTest() {
-        Assertions.assertEquals("A Fanciful Documentary of a Frisbee And a Lumberjack who must Chase a Monkey in A Shark Tank", film.getDescription());
+        Assertions.assertEquals("A Fast-Paced Documentary of a Pastry Chef" +
+                        " And a Dentist who must Pursue a Forensic Psychologist in The Gulf of Mexico"
+                , film.getDescription());
     }
 
     @Test
@@ -71,50 +58,45 @@ public class FilmTest {
         Assertions.assertEquals(2006, film.getReleaseYear());
     }
 
-//    @Test
-//    @DisplayName("Given id 4, get language id ")
-//    public void getLanguageId(){
-//        Assertions.assertEquals(1, film.getLanguage());
-//    }
 
     @Test
-    @DisplayName("Given id 4, return rental duration")
+    @DisplayName("Given id 5, return rental duration")
     public void getRentalDurationTest() {
-        Assertions.assertEquals(5, film.getRentalDuration());
+        Assertions.assertEquals(6, film.getRentalDuration());
     }
 
     @Test
-    @DisplayName("Given id 4, return rental rate")
+    @DisplayName("Given id 5, return rental rate")
     public void getRentalRateTest() {
         Assertions.assertEquals(2.99, film.getRentalRate());
     }
 
     @Test
-    @DisplayName("Given id 4, return length of film")
+    @DisplayName("Given id 5, return length of film")
     public void getLengthTest() {
-        Assertions.assertEquals(117, film.getLength());
+        Assertions.assertEquals(130, film.getLength());
     }
 
     @Test
-    @DisplayName("Given id 4, return the replacement cost")
+    @DisplayName("Given id 5, return the replacement cost")
     public void getReplacementCostTest() {
-        Assertions.assertEquals(26.99, film.getReplacementCost());
+        Assertions.assertEquals(22.99, film.getReplacementCost());
     }
 
     @Test
-    @DisplayName("Given id 4, return rating")
+    @DisplayName("Given id 5, return rating")
     public void getRatingTest() {
         Assertions.assertEquals("G", film.getRating());
     }
 
     @Test
-    @DisplayName("Given id 4, return the special features")
+    @DisplayName("Given id 5, return the special features")
     public void getSpecialFeaturesTest() {
-        Assertions.assertEquals("Commentaries,Behind the Scenes", film.getSpecialFeatures());
+        Assertions.assertEquals("Deleted Scenes", film.getSpecialFeatures());
     }
 
     @Test
-    @DisplayName("Given id 4, return last update")
+    @DisplayName("Given id 5, return last update")
     public void getLastUpdateTest() {
         Assertions.assertEquals("2006-02-15T05:03:42Z", film.getLastUpdate());
     }
@@ -160,7 +142,7 @@ public class FilmTest {
     public void deleteFilmTest() {
         HttpRequest request = HttpRequest
                 .newBuilder()
-                .uri(URI.create("http://localhost:8080/sakila/films/delete/4"))
+                .uri(URI.create("http://localhost:8080/sakila/films/delete/998"))
                 .DELETE()
                 .build();
         HttpClient client = HttpClient.newHttpClient();
@@ -172,6 +154,43 @@ public class FilmTest {
         }
         Assertions.assertEquals(200, response.statusCode());
     }
+
+    @Test
+    @DisplayName("Updating a film")
+    public void updateFilmTest() {
+        HttpRequest request = HttpRequest
+                .newBuilder()
+                .uri(URI.create("http://localhost:8080/sakila/films/update"))
+                .PUT(HttpRequest.BodyPublishers.ofString("""
+                        {
+                                 "id": 3,
+                                 "title": "CATCH ME IF YOU CAN",
+                                 "description": "An epic drama with Leonardo Di Caprio",
+                                 "releaseYear": 2006,
+                                 "language": {
+                                     "id": 1
+                                 },
+                                 "originalLanguage": null,
+                                 "rentalDuration": 3,
+                                 "rentalRate": 0.99,
+                                 "length": 115,
+                                 "replacementCost": 22.99,
+                                 "rating": "PG",
+                                 "specialFeatures": "Deleted Scenes,Behind the Scenes",
+                                 "lastUpdate": "2006-02-15T05:03:42Z"
+                             }
+                        """)).header("content-type", "application/json")
+                .build();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertEquals(200, response.statusCode());
+    }
+
 
 
 }
