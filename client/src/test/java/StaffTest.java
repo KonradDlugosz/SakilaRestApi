5078c602b0223a664032160841b70d992df6c253
KonradDlugosz <56347935+KonradDlugosz@users.noreply.github.com>
2021-12-17 00:22:10 +0000

    Added Test cases for Staff Operations

--- client/src/test/java/StaffTest.java
+++ client/src/test/java/StaffTest.java
@@ -4,18 +4,24 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static com.sprata.sakila.StaffRequests.*;
+import static com.sprata.sakila.util.Constants.LOGGER;
 
 public class StaffTest {
 
     public static Staff getOneStaff = null;
     public static Staff postStaff = null;
-
+    public static Staff putStaff = null;
+    public static Map<String,Boolean> isDeleted = null;
 
     @BeforeAll
     public static void getConnection(){
-        getOneStaff = getOneStaffJsonBody();
-        postStaff = postOneStaffJsonBody();
+        getOneStaff = getOneStaff();
+        postStaff = postNewStaff();
+        putStaff = putStaff();
+        isDeleted = deleteStaff();
     }
 
     /**
@@ -70,6 +76,12 @@ public class StaffTest {
         Assertions.assertEquals("2006-02-15T03:57:16Z", getOneStaff.getLastUpdate());
     }
 
+    @Test
+    @DisplayName("1.9 Given Id one, Return store id")
+    public void getStaffStore(){
+        Assertions.assertEquals(1, getOneStaff.getStore());
+    }
+
 
     /**
      * Tests for POST request
@@ -77,9 +89,129 @@ public class StaffTest {
 
     @Test
     @DisplayName("2.1 Given body to insert, Return first name")
-    public void postStaffName(){
+    public void postStaffFirstName(){
         Assertions.assertEquals("Konrad", postStaff.getFirstName());
+    }
 
+    @Test
+    @DisplayName("2.2 Given body to insert, Return last name")
+    public void postStaffLastName(){
+        Assertions.assertEquals("Dlugosz", postStaff.getLastName());
+    }
+
+    @Test
+    @DisplayName("2.3 Given body to insert, Return last address")
+    public void postStaffAddress(){
+        Assertions.assertEquals(4, postStaff.getAddress().getId());
+    }
+
+    @Test
+    @DisplayName("2.4 Given body to insert, Return email")
+    public void postStaffEmail(){
+        Assertions.assertEquals("konrad@sakilastaff.com", postStaff.getEmail());
+    }
+
+    @Test
+    @DisplayName("2.5 Given body to insert, Return store id")
+    public void postStaffStore(){
+        Assertions.assertEquals(1, postStaff.getStore());
+    }
+
+    @Test
+    @DisplayName("2.6 Given body to insert, Return status")
+    public void postStaffStatus(){
+        Assertions.assertTrue(postStaff.isActive());
+    }
+
+    @Test
+    @DisplayName("2.7 Given body to insert, Return username")
+    public void postStaffUsername(){
+        Assertions.assertEquals("konrad", postStaff.getUsername());
+    }
+
+    @Test
+    @DisplayName("2.7 Given body to insert, Return password")
+    public void postStaffPassword(){
+        Assertions.assertEquals("konrad1234", postStaff.getPassword());
+    }
+
+    @Test
+    @DisplayName("2.8 Given body to insert, Return last update")
+    public void postStaffLastUpdate(){
+        Assertions.assertEquals("2021-02-15T03:57:16Z", postStaff.getLastUpdate());
+    }
+
+    /**
+     * Tests for PUT request
+     */
+
+    @Test
+    @DisplayName("3.1 Given PUT request, return updated first name")
+    public void putStaffFirstName(){
+        Assertions.assertEquals("Alan", putStaff.getFirstName());
+    }
+
+    @Test
+    @DisplayName("3.2 Given PUT request, return updated last name")
+    public void putStaffLastName(){
+        Assertions.assertEquals("Edwards", putStaff.getLastName());
+    }
+
+    @Test
+    @DisplayName("3.3 Given PUT request, return updated address")
+    public void putStaffAddress(){
+        Assertions.assertEquals(1, putStaff.getAddress().getId());
+    }
+
+    @Test
+    @DisplayName("3.4 Given PUT request, return updated email")
+    public void putStaffEmail(){
+        Assertions.assertEquals("Alan@sakilastaff.com", putStaff.getEmail());
+    }
+
+    @Test
+    @DisplayName("3.5 Given PUT request, return updated email")
+    public void putStaffStore(){
+        Assertions.assertEquals(1, putStaff.getStore());
+    }
+
+    @Test
+    @DisplayName("3.6 Given PUT request, return updated status")
+    public void putStaffStatus(){
+        Assertions.assertFalse( putStaff.isActive());
+    }
+
+    @Test
+    @DisplayName("3.7 Given PUT request, return updated username")
+    public void putStaffUsername(){
+        Assertions.assertEquals("Alan", putStaff.getUsername());
+    }
+
+    @Test
+    @DisplayName("3.8 Given PUT request, return updated password")
+    public void putStaffPassword(){
+        Assertions.assertEquals("AlanEd201", putStaff.getPassword());
+    }
+
+    @Test
+    @DisplayName("3.8 Given PUT request, return last updated")
+    public void putStaffLastUpdated(){
+        Assertions.assertEquals("2021-02-15T03:57:16Z", putStaff.getLastUpdate());
+    }
+
+    /**
+     * Tests for DELETE request
+     */
+
+    @Test
+    @DisplayName("4.1 Given delete request, Return successfully response")
+    public void deleteStaffB(){
+        if(isDeleted.containsValue(false)){
+            LOGGER.warn("Record doesn't exists, can't delete : TEST CASE 4.1");
+        } else {
+            Assertions.assertTrue(isDeleted.containsValue(true));
+            LOGGER.info("Record deleted");
+        }
     }
 
 }
